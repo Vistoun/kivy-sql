4b534e7c3a84d326a3ba86c9efb4ccee50906e4c
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2021-05-10 09:44:42 +0200

    text update

--- modules/opravari.py
+++ modules/opravari.py
@@ -17,11 +17,8 @@ class ModelContent(BoxLayout):
 
 class ModelDialog(MDDialog):
     def __init__(self, *args, **kwargs):
-
         super(ModelDialog, self).__init__(
-
             type="custom",
-
             content_cls=ModelContent(),
             title='Nový model',
             size_hint=(.8, 1),
@@ -51,13 +48,10 @@ class OpravarContent(BoxLayout):
 
 class OpravarDialog(MDDialog):
     def __init__(self, *args, **kwargs):
-
         super(OpravarDialog, self).__init__(
-
             type="custom",
-
             content_cls=OpravarContent(),
-            title='Nový opravar',
+            title='Nový opravář',
             size_hint=(.8, 1),
 
             buttons=[
@@ -79,7 +73,6 @@ class OpravarDialog(MDDialog):
 class OpravaContent(BoxLayout):
     def __init__(self, id, *args, **kwargs):
         super().__init__(**kwargs)
-
         if id:
             oprava = vars(app.opravy.database.read_oprava_by_id(id))
         else:
@@ -168,14 +161,12 @@ class OpravaDialog(MDDialog):
 class MyItem(TwoLineAvatarIconListItem):
     def __init__(self, item, *args, **kwargs):
         super(MyItem, self).__init__()
-
         self.id = item['id']
         self.text = item['popis']
         self.secondary_text = item['model'] + ", " + item['opravar']
         self._no_ripple_effect = True
 
         self.image = ImageLeftWidget()
-
         self.image.source = f"images/{item['popis']}.png"
         self.add_widget(self.image)
 
@@ -183,17 +174,10 @@ class MyItem(TwoLineAvatarIconListItem):
         self.add_widget(self.icon)
 
     def on_press(self):
-        """
-        Metoda je vyvolána po stisknutí tlačítka v oblasti widgetu
-        Otevře se dialogové okno pro editaci osobních dat
-        """
         self.dialog = OpravaDialog(id=self.id)
         self.dialog.open()
 
     def on_delete(self, *args):
-        """
-        Metoda je vyvolána po kliknutí na ikonu koše - vymazání záznamu
-        """
         yes_button = MDFlatButton(
             text='Ano', on_release=self.yes_button_release)
         no_button = MDFlatButton(text='Ne', on_release=self.no_button_release)
@@ -225,35 +209,35 @@ class Opravy(BoxLayout):
 
         button_box = BoxLayout(orientation='horizontal', size_hint_y=0.1)
         new_oprava_btn = MDFillRoundFlatIconButton()
-        new_oprava_btn.text = "Nova oprava"
+        new_oprava_btn.text = "Nová oprava"
         new_oprava_btn.icon = "plus"
         new_oprava_btn.icon_color = [0.9, 0.9, 0.9, 1]
         new_oprava_btn.text_color = [0.9, 0.9, 0.9, 1]
         new_oprava_btn.md_bg_color = [0, 0.5, 0.8, 1]
         new_oprava_btn.font_style = "Button"
-        new_oprava_btn.pos_hint = {"center_x": .5}
+        new_oprava_btn.pos_hint = {"center_y": .5}
         new_oprava_btn.on_release = self.on_create_oprava
         button_box.add_widget(new_oprava_btn)
 
         new_opravar_btn = MDFillRoundFlatIconButton()
-        new_opravar_btn.text = "Nový opravar"
+        new_opravar_btn.text = "Nový opravář"
         new_opravar_btn.icon = "plus"
         new_opravar_btn.icon_color = [0.9, 0.9, 0.9, 1]
         new_opravar_btn.text_color = [0.9, 0.9, 0.9, 1]
         new_opravar_btn.md_bg_color = [0, 0.2, 0.5, 1]
         new_opravar_btn.font_style = "Button"
-        new_opravar_btn.pos_hint = {"center_x": .6}
+        new_opravar_btn.pos_hint = {"center_y": .5}
         new_opravar_btn.on_release = self.on_create_opravar
         button_box.add_widget(new_opravar_btn)
 
         new_model_btn = MDFillRoundFlatIconButton()
-        new_model_btn.text = "Nový Model"
+        new_model_btn.text = "Nový model"
         new_model_btn.icon = "plus"
         new_model_btn.icon_color = [0.9, 0.9, 0.9, 1]
         new_model_btn.text_color = [0.9, 0.9, 0.9, 1]
         new_model_btn.md_bg_color = [0.8, 0.5, 0, 1]
         new_model_btn.font_style = "Button"
-        new_model_btn.pos_hint = {"center_x": .7}
+        new_model_btn.pos_hint = {"center_y": .5}
         new_model_btn.on_release = self.on_create_model
         button_box.add_widget(new_model_btn)
         self.add_widget(button_box)
